c435a013691c082059638bddd931963a1d8b492a
ialexanderbrito <xanderzinho26@gmail.com>
2021-02-24 22:08:03 -0300

    :sparkles: Adicionado Hook de Countdown

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,20 +1,44 @@
+import { useState, useEffect } from 'react';
 import { Container, Button } from './styles';
 
 const Countdown = () => {
+  const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  function startCountdown() {
+    setActive(true);
+  }
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [active, time]);
+
   return (
     <div>
       <Container>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </Container>
-      <Button type="button">Iniciar um ciclo</Button>
+      <Button type="button" onClick={startCountdown}>
+        Iniciar um ciclo
+      </Button>
     </div>
   );
 };
