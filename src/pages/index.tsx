fe187d8eb78cd87cca6fbc7a2f157f0fc1695b8a
ialexanderbrito <xanderzinho26@gmail.com>
2021-02-24 21:30:04 -0300

    :sparkles: Adicionado componente em tela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import CompletedChallenges from '../components/CompletedChallenges';
 import ExperienceBar from '../components/ExperienceBar';
 import Profile from '../components/Profile';
 import { Container } from '../styles/pages/index';
@@ -11,6 +12,7 @@ export default function Home() {
         <section>
           <div>
             <Profile />
+            <CompletedChallenges />
           </div>
 
           <div></div>
