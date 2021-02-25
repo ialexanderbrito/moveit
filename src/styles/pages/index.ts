74610e7d611d4c791ba485118d7600f3a12b3790
ialexanderbrito <xanderzinho26@gmail.com>
2021-02-24 21:20:22 -0300

    :sparkles: Adicionado section

--- src/styles/pages/index.ts
+++ src/styles/pages/index.ts
@@ -8,4 +8,12 @@ export const Container = styled.div`
 
   display: flex;
   flex-direction: column;
+
+  section {
+    flex: 1;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 6.25rem;
+    align-content: center;
+  }
 `;
